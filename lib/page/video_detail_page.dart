8dc3f05f5874a53a171fee483c41715164e1425e
lyq12345 <971484814@qq.com>
2021-11-10 21:41:05 -0800

    init hi_video_controls

--- lib/page/video_detail_page.dart
+++ lib/page/video_detail_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/model/video_model.dart';
+import 'package:flutter_application_1/widget/video_view.dart';
 
 class VideoDetailPage extends StatefulWidget {
   final VideoMo videoModel;
@@ -16,10 +17,21 @@ class _VideoDetailPageState extends State<VideoDetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        child: Text('视频详情页, vid:${widget.videoModel.vid}'),
-      ),
+        appBar: AppBar(),
+        body: Column(
+          children: [
+            Text('视频详情页, vid:${widget.videoModel.vid}'),
+            Text('视频详情页, vid:${widget.videoModel.title}'),
+            _videoView(),
+          ],
+        ));
+  }
+
+  _videoView() {
+    var model = widget.videoModel;
+    return VideoView(
+      model.url!,
+      cover: model.cover,
     );
   }
 }
